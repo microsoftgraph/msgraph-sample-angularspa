420f0729d06feb8ebad90ca7e686b941975ea12f
Jason Johnston <jasonjoh@users.noreply.github.com>
2018-12-05 15:50:34 -0500

    Progress so far
    Trying to get navbar working with just Bootstrap

--- demos/01-create-app/graph-tutorial/src/app/app.module.ts
+++ demos/01-create-app/graph-tutorial/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
