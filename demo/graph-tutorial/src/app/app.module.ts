8c362658d8e87f7ca42ce53bacff9640d8091d12
Jason Johnston <jasonjoh@users.noreply.github.com>
2020-03-13 14:51:04 -0400

    UI tweaks

--- demo/graph-tutorial/src/app/app.module.ts
+++ demo/graph-tutorial/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 import { faUserCircle } from '@fortawesome/free-regular-svg-icons';
@@ -11,9 +11,6 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
 import { AlertsComponent } from './alerts/alerts.component';
-
-library.add(faExternalLinkAlt);
-library.add(faUserCircle);
 
 @NgModule({
   declarations: [
@@ -31,4 +28,9 @@ library.add(faUserCircle);
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    // Register the FontAwesome icons used by the app
+    library.addIcons(faExternalLinkAlt, faUserCircle);
+  }
+ }
