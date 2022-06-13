4f1d69caedad0e71fd2a08260ff753b9508b1303
Jason Johnston <jasonjoh@users.noreply.github.com>
2022-06-13 12:50:06 -0400

    Delete old project

--- demo/graph-tutorial/src/oauth.example.ts
+++
@@ -1,9 +0,0 @@
-export const OAuthSettings = {
-  appId: 'YOUR_APP_ID_HERE',
-  redirectUri: 'http://localhost:4200',
-  scopes: [
-    "user.read",
-    "mailboxsettings.read",
-    "calendars.readwrite"
-  ]
-};
